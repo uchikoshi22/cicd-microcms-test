17157f396287dd6b685e1e0a72f68921827c5580
zuzuta22 <yukitaka.uchikoshi@gmail.com>
2024-09-14 14:06:09 +0900

    topページ再修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,24 @@
-// app/page.tsx または pages/index.tsx
+// app/page.tsx または app/blog/page.tsx
 
 import { MyClient } from "./_src/libs/client";
 
 export async function generateStaticParams() {
   const response = await MyClient.get({ endpoint: "blog" });
 
-  return {
-    paths: response.contents.map((item: any) => ({
-      params: { id: item.id }
-    })),
-    fallback: false
-  };
+  // すべてのページのパスを指定
+  return response.contents.map((item: any) => ({
+    id: item.id
+  }));
 }
 
-export async function getStaticProps() {
+const Home = async() => {
   const response = await MyClient.get({ endpoint: "blog" });
 
-  return {
-    props: {
-      contents: response.contents
-    }
-  };
-}
-
-const Home = ({ contents }: { contents: any[] }) => {
   return (
     <>
-      {contents ? (
+      {response.contents ? (
         <ul>
-          {contents.map((item: any) => (
+          {response.contents.map((item: any) => (
             <li key={item.id}>
               <a href={`/${item.id}.html`}>
                 {item.title}
