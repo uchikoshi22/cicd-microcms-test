ac9e4b5a7dbc49f2cee33a36993d0386216787ee
zuzuta22 <yukitaka.uchikoshi@gmail.com>
2024-09-14 13:16:12 +0900

    variable modified

--- app/_src/libs/client.ts
+++ app/_src/libs/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from 'microcms-js-sdk';
 
-console.log('Service ID:', process.env.SERVICE_ID);
+console.log('Service ID:', process.env.MICROCMS_SERVICE_DOMAIN);
 console.log('API Key:', process.env.API_KEY);
 
 export const MyClient = createClient({
-  serviceDomain: process.env.SERVICE_ID || "",
-  apiKey: process.env.API_KEY || "",
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || "",
+  apiKey: process.env.MICROCMS_API_KEY || "",
 });
