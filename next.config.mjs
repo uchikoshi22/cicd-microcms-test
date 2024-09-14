7947f9ebdbbb1159f03c513cfee9ba18184de253
zuzuta22 <yukitaka.uchikoshi@gmail.com>
2024-09-14 13:01:54 +0900

    next.config修正

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   env: {
-    SERVICE_ID: process.env.PUBLIC_MICROCMS_SERVICE_ID,
+    SERVICE_ID: process.env.SERVICE_ID,
     API_KEY: process.env.API_KEY,
     PUBLIC_SITE_URL: process.env.PUBLIC_SITE_URL,
   },
