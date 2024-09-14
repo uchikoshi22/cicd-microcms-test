937682e918a84017a07d9e4d7f57d000a27a4f0d
zuzuta22 <yukitaka.uchikoshi@gmail.com>
2024-09-14 12:59:06 +0900

    yaml修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  env: {
+    SERVICE_ID: process.env.PUBLIC_MICROCMS_SERVICE_ID,
+    API_KEY: process.env.API_KEY,
+    PUBLIC_SITE_URL: process.env.PUBLIC_SITE_URL,
+  },
 };
 
 export default nextConfig;
