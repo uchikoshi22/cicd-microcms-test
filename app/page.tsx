8ebaf1e37ef428ac21999bb305b2473ff4687d10
zuzuta22 <yukitaka.uchikoshi@gmail.com>
2024-09-14 14:03:38 +0900

    topページ静的ビルド用調整

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,42 @@
+// app/page.tsx または pages/index.tsx
 
 import { MyClient } from "./_src/libs/client";
 
-const Home = async() => {
+export async function generateStaticParams() {
   const response = await MyClient.get({ endpoint: "blog" });
-  console.log(response)
 
+  return {
+    paths: response.contents.map((item: any) => ({
+      params: { id: item.id }
+    })),
+    fallback: false
+  };
+}
+
+export async function getStaticProps() {
+  const response = await MyClient.get({ endpoint: "blog" });
+
+  return {
+    props: {
+      contents: response.contents
+    }
+  };
+}
+
+const Home = ({ contents }: { contents: any[] }) => {
   return (
     <>
-    {response ? (
-      <ul>
-        {response.contents.map((item:any, key:number) => (
-        <li key={key}>
-          <a href={`/${item.id}.html`}>
-            {item.title}
-          </a>
-        </li>
-      ))}
-      </ul>
-    ) : null}
+      {contents ? (
+        <ul>
+          {contents.map((item: any) => (
+            <li key={item.id}>
+              <a href={`/${item.id}.html`}>
+                {item.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      ) : null}
     </>
   );
 }
